1673d91830096a7658e0442e42156366a099f7cc
Vlad <vlad-gezik@mail.ru>
2026-03-13 00:01:00 +0500

    fix reviewer comments

--- api_tests/data.py
+++ api_tests/data.py
@@ -12,8 +12,3 @@ def generate_password():
 
 def generate_name():
     return ''.join(random.choices(string.ascii_lowercase, k=6))
-
-
-INGREDIENT = ["61c0c5a71d1f82001bdaaa6d"]
-
-INVALID_INGREDIENT = ["invalid_hash"]
--- api_tests/helpers.py
+++ api_tests/helpers.py
@@ -1,26 +1,21 @@
 import requests
-import allure
 
 BASE_URL = "https://stellarburgers.education-services.ru/api"
 
 
-@allure.step("Создание пользователя")
 def create_user(data):
     return requests.post(f"{BASE_URL}/auth/register", json=data)
 
 
-@allure.step("Логин пользователя")
 def login_user(data):
     return requests.post(f"{BASE_URL}/auth/login", json=data)
 
 
-@allure.step("Удаление пользователя")
 def delete_user(token):
     headers = {"Authorization": token}
     return requests.delete(f"{BASE_URL}/auth/user", headers=headers)
 
 
-@allure.step("Создание заказа")
 def create_order(token, ingredients):
     headers = {"Authorization": token}
     data = {"ingredients": ingredients}
